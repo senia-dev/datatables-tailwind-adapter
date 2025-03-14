4deead2e1d6e1e04a34742493b3702d48e1783ce
Senia <arsenii.konanykhin@gmail.com>
2025-03-14 02:02:48 +0100

    Adding custom SVGs for SearchPanes controls

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { AdapterConfig } from "./extensions/config";
 
 import { Adapter } from "./src/datatables";
 import { Structure } from "./src/structure";
+import { svg } from "./extensions/svg";
 
 export class TailwindAdapter {
     // Public prebuilt object
@@ -88,6 +89,50 @@ export class TailwindAdapter {
             DataTable.SearchPanes.classes,
             this.#validateStructure(this.DataTableT.SearchPanes, 'search_panes', this.#DataTableR.SearchPanes).classes
         );
+
+        $(document).on('preInit.dt', function (e, settings) {
+            let $table = e.dt;
+
+            // Change icons in SearchPanes buttons
+            $table.on('init', function () {
+                // SearchPanes container
+                var $sp = $table.searchPanes.container();
+
+                if ($sp.length > 0) {
+                    $sp.find('div>div.dtsp-searchPane').each((key, element) => {
+                        // Search panes - Search button
+                        var $searchButton = $(element).children('div').find('button.dtsp-search');
+                        if ($searchButton.length > 0 && $searchButton.find('svg').length == 0) {
+                            $searchButton.html(svg.search);
+                        }
+
+                        // Search panes - Clear button (x)
+                        var $clearButton = $(element).children('div').find('button.dtsp-clear-button');
+                        if ($clearButton.length > 0 && $clearButton.find('svg').length == 0) {
+                            $clearButton.html(svg.clear);
+                        }
+
+                        // Search panes - Collapse button (^)
+                        var $collapseButton = $(element).children('div').find('button.dtsp-collapse-button');
+                        if ($collapseButton.length > 0 && $collapseButton.find('svg').length == 0) {
+                            $collapseButton.html(svg.collapse);
+                        }
+
+                        // Search panes - Sort alphabetical button
+                        var $nameButton = $(element).children('div').find('button.dtsp-nameButton');
+                        if ($nameButton.length > 0 && $nameButton.find('svg').length == 0) {
+                            $nameButton.html(svg.alphabetical);
+                        }
+
+                        // Search panes - Sort count button
+                        var $countButton = $(element).children('div').find('button.dtsp-countButton');
+                        if ($countButton.length > 0 && $countButton.find('svg').length == 0) {
+                            $countButton.html(svg.numeric);
+                        }
+                    });
+                }
+            });
+        });
 
         return this;
     }
